a8152106c7b91c400e1c72a34f676ac0d8ed65ec
chiichen <chiichen@qq.com>
2024-05-03 00:24:54 +0800

    chore(libvnc-sys):  ignore the warnings generated by binding codes

--- libvnc-sys/src/rfb/mod.rs
+++ libvnc-sys/src/rfb/mod.rs
@@ -1,3 +1,7 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(improper_ctypes)]
 include!(concat!(std::env!("OUT_DIR"), "/rfb.rs"));
 
 pub fn rfb_get_screen(
