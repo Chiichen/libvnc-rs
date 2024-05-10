25bddd9f49bcf40796f114bbbad7fba07e279a8c
chiichen <chiichen@qq.com>
2024-05-11 00:15:09 +0800

    feat(libvnc-sys): support build from source & add corresponding ci build test

--- libvnc-sys/build.rs
+++ libvnc-sys/build.rs
@@ -1,6 +1,11 @@
 use std::env;
 use std::path::PathBuf;
 
+#[cfg(not(any(feature = "pkg", feature = "source")))]
+compile_error!("You need to specify at least one feature (build from pkg or source)");
+#[cfg(all(feature = "pkg", feature = "source"))]
+compile_error!("You can not specify both feature (build from pkg or source)");
+
 #[cfg(feature = "pkg")]
 fn bindgen_vncserver() {
     let libvncserver =
@@ -32,6 +37,36 @@ fn bindgen_vncserver() {
             "-I{}",
             libvncserver.include_paths[0].to_str().unwrap(),
         ))
+        .generate()
+        .expect("unable to generate rfb bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("rfb.rs"))
+        .expect("couldn't write bindings!");
+}
+#[cfg(feature = "source")]
+fn bindgen_vncserver() {
+    #[cfg(target_os = "android")]
+    compile_error!("Unsupported Target Android");
+
+    let mut config = cmake::Config::new("libvncserver");
+    #[cfg(target_os = "windows")]
+    config.define(
+        "CMAKE_TOOLCHAIN_FILE",
+        "../cmake/Toolchain-cross-mingw32-linux.cmake",
+    );
+    let dst = config.build();
+    println!("cargo:rustc-link-lib=dylib={}", "vncserver");
+    println!("cargo:rustc-link-lib=dylib=vncclient"); //There's no libvncclient , so we need to specify the vncclient manually
+    println!("cargo:rustc-link-search=native={}", dst.display());
+    let rfb_header = format!("{}/{}", "./libvncserver/include", "rfb/rfb.h");
+    let rfbclient_header = format!("{}/{}", "./libvncserver/include", "rfb/rfbclient.h");
+    let bindings = bindgen::Builder::default()
+        .header(rfb_header)
+        .header(rfbclient_header)
+        .use_core()
+        .clang_arg(format!("-I{}", "./libvncserver/include"))
         .generate()
         .expect("unable to generate rfb bindings");
 
